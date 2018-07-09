c004d56373e2347f90cc25a70a6d7a754507ba38
khuangaf <huangkh19951228@gmail.com>
2018-07-09 16:03:25 +0800

    fix bug, able to generate srt

--- src/csv_to_srt.py
+++ src/csv_to_srt.py
@@ -19,12 +19,15 @@ def get_all_inputs(dir):
     
 def get_time(id, sample_rate, start_time):
     sample_index = int(id.split('-')[-2])
+#     print(sample_rate)
     return sample_index / sample_rate + start_time
 
 
 def second2timecode(time):
     hours = time // 3600
+    time -= 3600 * hours
     minutes = time // 60
+    time -= 60 * minutes
     secs = np.floor(time % 60)
     msecs = (time % 60 - secs) * 1000
     timecode = "%02d:%02d:%02d,%03d" % (hours, minutes, secs, msecs)
@@ -43,7 +46,7 @@ def main():
     videos_dir = args.videos_dir
     os.makedirs(args.srts_dir, exist_ok=True)
     video_csvs = get_all_inputs(csvs_dir)
-    for video_csv in video_csvs:
+    for video_csv in tqdm(video_csvs):
         video_id = video_csv.split('.csv')[0]
 #         print(f'{csvs_dir}/{video_csv}')
         df = pd.read_csv(f'{csvs_dir}/{video_csv}')
@@ -51,30 +54,38 @@ def main():
         sample_rate = get_sample_rate(first_sample)
         path_video = f'{videos_dir}/{video_id}.mp4'
         duration = get_duration(path_video)
-        start_time = 0.2 * duration
-        current_subtitle= None
         
+        current_subtitle= None
+        # seconds per frame
+        
+        spf = 1.0 / sample_rate
+        
+        # records the cut-off of the video
+        video_start_time = 0.2 * duration  - spf*2
+        subtitle_start_time = video_start_time
         # get only subtitles with confidence > 0.95
         df = df.loc[df.confidence >= 0.95, :].set_index('id')
         
         # a string that gathers subtitles for srt output
         subtitles = ''
         subtitle_count = 1
-        for index, row in tqdm(df.iterrows()):
+        for index, row in df.iterrows():
             if current_subtitle == None:
                 
                 current_subtitle = row.prediction
 #                 continue
             elif current_subtitle == row.prediction:
-                end_time = get_time(index, sample_rate, start_time)
+                subtitle_end_time = get_time(index, sample_rate, video_start_time) 
             elif current_subtitle != row.prediction:
-                start_time = get_time(index, sample_rate, start_time)
+                
                 subtitles += f'{subtitle_count}\n'
-                subtitles += second2timecode(start_time) + ' --> ' + second2timecode(end_time) + '\n'
+                subtitles += second2timecode(subtitle_start_time) + ' --> ' + second2timecode(subtitle_end_time) + '\n'
                 subtitles += current_subtitle + '\n\n'
                 current_subtitle = row.prediction
+                subtitle_start_time = get_time(index, sample_rate, video_start_time) - spf
+                subtitle_end_time = get_time(index, sample_rate, video_start_time) 
                 subtitle_count +=1
-#         print(subtitles)
+
         with open(f'{srts_dir}/{video_id}.srt', 'wb') as f:
             f.write(subtitles.encode('utf-8'))
     
